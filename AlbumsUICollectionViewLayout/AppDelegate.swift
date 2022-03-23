08fb443620d2e74eb0148dbfbf6806ab9f2a47d7
Ilya Volkov <illvolkov@bk.ru>
2022-03-23 22:46:58 +0500

    Add TabBarController and missing resources

--- AlbumsUICollectionViewLayout/AppDelegate.swift
+++ AlbumsUICollectionViewLayout/AppDelegate.swift
@@ -14,6 +14,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        let tabBarController = UITabBarController()
+        tabBarController.tabBar.backgroundColor = .clear
+        
+        let mediaLibraryViewController = UIViewController()
+        let forYouViewController = UIViewController()
+        let albumsViewController = ViewController()
+        let searchViewController = UIViewController()
+        
+        let albumsNavigationController = UINavigationController(rootViewController: albumsViewController)
+        
+        
+        
+        mediaLibraryViewController.tabBarItem = UITabBarItem(title: "Медиатека",
+                                                             image: UIImage(systemName: "photo.fill.on.rectangle.fill"),
+                                                             tag: 0)
+        forYouViewController.tabBarItem = UITabBarItem(title: "Для вас",
+                                                       image: UIImage(systemName: "list.bullet.rectangle.portrait.fill"),
+                                                       tag: 1)
+        albumsViewController.tabBarItem = UITabBarItem(title: "Альбомы",
+                                                       image: UIImage(systemName: "rectangle.stack.fill"),
+                                                       tag: 2)
+        searchViewController.tabBarItem = UITabBarItem(title: "Поиск",
+                                                       image: UIImage(systemName: "magnifyingglass"),
+                                                       tag: 3)
+        
+        tabBarController.setViewControllers([mediaLibraryViewController,
+                                             forYouViewController,
+                                             albumsNavigationController,
+                                             searchViewController], animated: false)
+        
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
