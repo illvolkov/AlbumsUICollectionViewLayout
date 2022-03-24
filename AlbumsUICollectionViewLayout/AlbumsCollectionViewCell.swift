a85d4975b29a45139f9a9461e1d3bcb27c845beb
Ilya Volkov <illvolkov@bk.ru>
2022-03-25 03:55:30 +0500

    Setup cell display

---
+++ AlbumsUICollectionViewLayout/AlbumsCollectionViewCell.swift
@@ -0,0 +1,142 @@
+//
+//  AlbumsCollectionViewItem.swift
+//  AlbumsUICollectionViewLayout
+//
+//  Created by Ilya Volkov on 24.03.2022.
+//
+
+import Foundation
+import UIKit
+
+final class AlbumsCollectionViewCell: UICollectionViewListCell {
+    
+    static let identifier = "AlbumsCollectionViewCell"
+        
+    private lazy var albumImage: UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
+    
+    private lazy var peopleImageOne = createPeopleImage(with: UIImage(named: "people.image.one"))
+    private lazy var peopleImageTwo = createPeopleImage(with: UIImage(named: "people.image.two"))
+    private lazy var peopleImageThree = createPeopleImage(with: UIImage(named: "people.image.three"))
+    private lazy var peopleImageFour = createPeopleImage(with: UIImage(named: "people.image.four"))
+    
+    private lazy var albumNameLabel: UILabel = {
+        let label = UILabel()
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var numberOfPhotoLabel: UILabel = {
+        let label = UILabel()
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .left
+        label.alpha = 0.8
+        return label
+    }()
+    
+    private lazy var heartIcon: UIImageView = {
+        let icon = UIImageView()
+        icon.image = UIImage(systemName: "heart.fill")
+        icon.tintColor = .white
+        return icon
+    }()
+    
+    private func createPeopleImage(with peopleImage: UIImage?) -> UIImageView {
+        let imageView = UIImageView()
+        
+        imageView.layer.cornerRadius = 12
+        imageView.image = peopleImage
+        
+        return imageView
+    }
+    
+    private func setupDisplay(with model: AlbumItem) {
+        if model.isPeopleItem {
+            contentView.addSubview(peopleImageOne)
+            contentView.addSubview(peopleImageTwo)
+            contentView.addSubview(peopleImageThree)
+            contentView.addSubview(peopleImageFour)
+            contentView.addSubview(albumNameLabel)
+            contentView.addSubview(numberOfPhotoLabel)
+            
+            peopleImageOne.translatesAutoresizingMaskIntoConstraints = false
+            peopleImageTwo.translatesAutoresizingMaskIntoConstraints = false
+            peopleImageThree.translatesAutoresizingMaskIntoConstraints = false
+            peopleImageFour.translatesAutoresizingMaskIntoConstraints = false
+            
+            peopleImageOne.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+            peopleImageTwo.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+            
+            peopleImageTwo.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+            peopleImageTwo.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+            
+            peopleImageThree.topAnchor.constraint(equalTo: peopleImageOne.bottomAnchor).isActive = true
+            peopleImageThree.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+            
+            peopleImageFour.topAnchor.constraint(equalTo: peopleImageTwo.bottomAnchor).isActive = true
+            peopleImageFour.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+            
+            albumNameLabel.translatesAutoresizingMaskIntoConstraints = false
+            albumNameLabel.topAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+            albumNameLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
+            
+            numberOfPhotoLabel.translatesAutoresizingMaskIntoConstraints = false
+            numberOfPhotoLabel.topAnchor.constraint(equalTo: albumNameLabel.bottomAnchor).isActive = true
+            numberOfPhotoLabel.widthAnchor.constraint(equalTo: albumNameLabel.widthAnchor).isActive = true
+        } else {
+            contentView.addSubview(albumImage)
+            contentView.addSubview(albumNameLabel)
+            contentView.addSubview(numberOfPhotoLabel)
+            
+            if model.isListItem {
+                albumImage.translatesAutoresizingMaskIntoConstraints = false
+                albumImage.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+                albumImage.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+                
+                albumNameLabel.translatesAutoresizingMaskIntoConstraints = false
+                albumNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+                albumNameLabel.leftAnchor.constraint(equalTo: albumImage.rightAnchor).isActive = true
+                
+                numberOfPhotoLabel.translatesAutoresizingMaskIntoConstraints = false
+                numberOfPhotoLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+                numberOfPhotoLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor,
+                                                          constant: -20).isActive = true
+            } else {
+                albumImage.translatesAutoresizingMaskIntoConstraints = false
+                albumImage.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+                albumImage.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+                albumImage.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+                albumImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+                
+                albumNameLabel.translatesAutoresizingMaskIntoConstraints = false
+                albumNameLabel.topAnchor.constraint(equalTo: albumImage.bottomAnchor).isActive = true
+                albumNameLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
+                
+                numberOfPhotoLabel.translatesAutoresizingMaskIntoConstraints = false
+                numberOfPhotoLabel.topAnchor.constraint(equalTo: albumNameLabel.bottomAnchor).isActive = true
+                numberOfPhotoLabel.widthAnchor.constraint(equalTo: albumNameLabel.widthAnchor).isActive = true
+                if model.isHeartIcon {
+                    contentView.addSubview(heartIcon)
+                    
+                    heartIcon.translatesAutoresizingMaskIntoConstraints = false
+                    heartIcon.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10).isActive = true
+                    heartIcon.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10).isActive = true
+                }
+            }
+        }
+
+    }
+    
+    func configure(with model: AlbumItem) {
+        albumImage.image = model.photo
+        albumImage.tintColor = model.isListItem ? .systemBlue : nil
+        albumNameLabel.text = model.albumName
+        albumNameLabel.textColor = model.isListItem ? .systemBlue : nil
+        numberOfPhotoLabel.text = model.numberOfPhoto
+        setupDisplay(with: model)
+    }
+
+}
--- AlbumsUICollectionViewLayout/ViewController.swift
+++ AlbumsUICollectionViewLayout/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
         
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "item")
+        collectionView.register(AlbumsCollectionViewCell.self, forCellWithReuseIdentifier: AlbumsCollectionViewCell.identifier)
         
         return collectionView
     }()
@@ -60,7 +60,7 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
         
-    var models = [Section]()
+    var models = AlbumModel.configure()
 }
 
 extension ViewController: UICollectionViewDataSource {
@@ -74,7 +74,16 @@ extension ViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let model = models[indexPath.section].options[indexPath.row]
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: AlbumsCollectionViewCell.identifier,
+            for: indexPath) as? AlbumsCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        if model.isListItem {
+            cell.accessories = [.disclosureIndicator()]
+        }
+        cell.configure(with: model)
         return cell
     }
 }
